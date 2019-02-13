81f0c60c4bb730a795a14b7ceeb5f2ff0b1257b2
Michael Jaison Gnanasekar <michaeljaison@gmj-MacbookPro.local>
2019-02-13 15:49:43 -0800

    git & zsh configs

---
+++ zsh/custom/aliases.zsh
@@ -0,0 +1,10 @@
+# General
+alias c='clear'
+alias ..='cd ..'
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
+alias ..5='cd ../../../../..'
+
+# Git (Additional to zsh aliases)
+alias gs='git status'
